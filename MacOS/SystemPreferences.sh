08246908c311d5e319f929be0f6dffba4872909e
Collin Robison <collinrobison@gmail.com>
2025-11-14 23:44:03 -0600

    feat: update settings for cSpell and enhance System Preferences script for key repeating

--- MacOS/SystemPreferences.sh
+++ MacOS/SystemPreferences.sh
@@ -25,4 +25,15 @@ defaults write com.apple.finder ShowStatusBar -bool true
 
 echo "Show status bar setup complete."
 
+# enable key repeating 
+
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      # For VS Code Insider
+defaults write com.vscodium ApplePressAndHoldEnabled -bool false                      # For VS Codium
+defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   # For VS Codium Exploration users
+defaults write com.exafunction.windsurf ApplePressAndHoldEnabled -bool false          # For Windsurf
+defaults delete -g ApplePressAndHoldEnabled                                         # If necessary, reset global default
+
+echo "Key repeating settings complete for VS Code."
+
 killall Finder;
